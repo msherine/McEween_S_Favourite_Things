7e6bbb50c63ecbf9abdca84c1daf4352100bbfc3
msherine <90708400+msherine@users.noreply.github.com>
2022-12-07 18:07:20 -0500

    worked on car thumbs

--- js/main.js
+++ js/main.js
@@ -40,10 +40,4 @@ import lightBox from './modules/lightbox.js';
 
     }).mount('#app')
 
-    function showMenu() {
-        document.querySelector('.navigation').classList.toggle('active');
-        document.querySelector('.menu').classList.toggle('hide');
-        document.querySelector('.close').classList.toggle('show');
-    }
-
 })();
--- js/modules/lightbox.js
+++ js/modules/lightbox.js
@@ -10,12 +10,17 @@ export default{
    
     <img @click="closeLB" src="images/close_icon.svg">
     <article>
-    <img :src='"images/" + model.model_img' alt="car thumb image">
+    <img :src='"images/" + model.model_pic' alt="car thumb image">
 
     <div class="model_data">
     <h2> {{ model.model_type}} </h2>
-    <h3> {{ model.model_price}} </h3>
-    <h4> {{ model.model_hp}} </h4>
+    <h3> All inclusive price : {{ model.model_price}} </h3>
+    <p> Monthly payment starting from : {{ model.model_dp}} </p>
+    <h4> Features : </h4>
+    <p><span>Colour :</span> {{ model.model_color}} </p>
+    <p><span>Top-speed :</span> {{ model.model_speed}} </p>
+    <p><span>Fuel consumption :</span> {{ model.model_fuel}} </p>
+    <p><span>Output :</span> {{ model.model_hp}} </p>
     </div>
 
     </article>
